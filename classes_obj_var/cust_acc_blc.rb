33e074fcf2eb87ab56cfad03bc5026e48a92d056
nitin gupta <nitinmangala@in.com>
2012-04-27 18:04:41 +0530

    customer account balance

--- classes_obj_var/cust_acc_blc.rb
+++ classes_obj_var/cust_acc_blc.rb
@@ -6,23 +6,25 @@ class Customer
 		@acc_no = @@acc_no+=1;
 	end
 	
-	def deposit(amt)
+	attr_reader :name, :balance, :acc_no;
+	
+	def deposit=(amt)
 		@balance += amt;
 	end
 	
-	def withdrawal(amt)
+	def withdrawal=(amt)
 		@balance -= amt;
-	end
-	
-	def detail
-		print @balance,"\t",@name,"\t",@acc_no;
 	end
 end
 
 customer1 = Customer.new('matz',1000)
-customer1.deposit(1000)
-puts customer1.detail;
+customer1.deposit = 1000
+print customer1.acc_no,"\t",customer1.name,"\t",customer1.balance,"\n",;
+customer1.withdrawal = 1000;
+print customer1.acc_no, "\t",customer1.name, "\t",customer1.balance,"\n";
 
 customer2 = Customer.new('john',1000)
-customer2.deposit(100)
-puts customer2.detail;
+customer2.deposit = 100;
+print customer2.acc_no, "\t",customer2.name, "\t",customer2.balance,"\n",;
+customer2.withdrawal = 1000;
+print customer2.acc_no, "\t",customer2.name, "\t",customer2.balance,"\n",;
