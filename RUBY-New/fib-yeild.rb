bebb76f6d41ab810d89b937682d4f11b4f5bd414
nitin gupta <nitinmangala@in.com>
2012-04-27 14:12:28 +0530

    Ruby.New Exercise

--- RUBY-New/fib-yeild.rb
+++
@@ -1,9 +0,0 @@
-def fib_series(max)
-	i1, i2 = 1, 1				# parallel assignment (i1 = 1 and i2 = 1)
-	while i1 <= max
-		yield i1
-		i1, i2 = i2, i1+i2
-	end
-	print "\n"
-end
-fib_series(1000) {|f| print f, " " }
