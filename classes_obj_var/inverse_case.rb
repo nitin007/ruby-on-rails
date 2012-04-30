adad28999c7d0be02a75a3e53e44961156ee5c7c
nitin gupta <nitinmangala@in.com>
2012-04-30 15:06:12 +0530

    inverting the case of text

--- classes_obj_var/inverse_case.rb
+++ classes_obj_var/inverse_case.rb
@@ -2,16 +2,14 @@ class String
 	def to_s
 		i=0;
 		self.each_char do |c|
-			yield c,c.match(/[a-z]/);
+			if c.match(/[a-z]/)
+				yield c.upcase;
+			else
+				yield c.downcase;
+			end
 		end
 	end
 end
 
-"hello WORLD".to_s do |c,f|
-	if f
-		print c.upcase;
-	else
-		print c.downcase;
-	end
-end
+"hello WORLD".to_s {|c| print c;}
 
