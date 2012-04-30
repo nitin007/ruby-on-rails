a7889d5ab2f22d4bb43cd0135a47c2c1f26a24bd
nitin gupta <nitinmangala@in.com>
2012-04-30 13:16:33 +0530

    pascal traingle

--- containers_blocks_iterators/pascal_triangle.rb
+++ containers_blocks_iterators/pascal_triangle.rb
@@ -1,32 +1,28 @@
 def pascal(n)
 	a=[];
-
+	b=[];
+	a.push(1,1);
+	b.push(1,1);
+		
 	for i in 0..n
-		num=1;
-		
-		(i-1).times do
-			num=num*11;
-		end
-		
-		if i==0
-			puts "1";
-		else
-
-			print "1 ";
-			if i>1
-				while num>0
-					a.push(num%10);
-					num=num/10.abs;
-				end
-				
-				for k in 0...a.length-1
-					print a[k]+a[k+1]," ";
-				end
+		if i<=1;
+			if i==0
+				puts a.first;
+			else		
+				print a.first," ",a.last,"\n";
 			end
-			print "1\n";
-			a.shift(a.length);
+
+		else
+			print a.first," ";
+			for j in 0...a.length-1
+				print a[j]+a[j+1]," ";
+				b[j+1] = a[j]+a[j+1];
+			end		
+			puts a.last;
+			b[b.length] = 1;
+			a=b.dup;
 		end
 	end
 end
 
-pascal(5);
+pascal(6);
