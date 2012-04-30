a1c7a82b5d925d95b089f84abbfa9bb6c75ece4a
nitin gupta <nitinmangala@in.com>
2012-04-30 15:18:06 +0530

    count chars(casewise,digits,special) using ranges

--- standard types/char_count_ranges.rb
+++ standard types/char_count_ranges.rb
@@ -3,9 +3,10 @@ def count_chars(str)
 	upper = str.count "A-Z";
 	digits = str.count "0-9";
 	special_chars = str.scan(/[^a-zA-Z0-9]/)
-	print "No. of lowercase characters: ",lower,"\n","No. of uppercase characters: ",upper,"\n","No. of digits: ",digits,"\n","No. of special characters: ",special_chars.length,"\n";
+	yield lower, upper, digits, special_chars.length;
 end
 
 print "enter some text: ";
 str = gets.chomp;
-count_chars(str);
+count_chars(str) {|l,u,d,s| 	print "No. of lowercase characters: ",l,"\n","No. of uppercase characters: ",u,"\n","No. of digits: ",d,"\n","No. of special characters: ",s,"\n";
+}
