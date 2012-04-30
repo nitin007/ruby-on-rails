c16cf23e31392a0d115552092cbdaa48455f992a
nitin gupta <nitinmangala@in.com>
2012-04-30 12:09:25 +0530

    changes done

--- RUBY-New/ltrOccurences.rb
+++ RUBY-New/ltrOccurences.rb
@@ -2,23 +2,17 @@
 # Create a blank hash, traverse over string - if intialize key with val if key not found, else increment key value 
 def occurences(text)
   # Join not needed 
-	sorted_text = text.chars.sort.join;
-
-	count=1;
-
 	i=0;
 
 	ltrhash = Hash.new;
 
 	while i<text.length
-		if sorted_text[i] == sorted_text[i+1]
-			count+=1;
-			i+=1;
+		if !ltrhash[text[i]]
+			ltrhash[text[i]]=1;
 		else
-			ltrhash[sorted_text[i]] = count;
-			i+=1;
-			count = 1;
+			ltrhash[text[i]]+=1;
 		end
+		i+=1;
 	end
 	puts ltrhash;
 end
