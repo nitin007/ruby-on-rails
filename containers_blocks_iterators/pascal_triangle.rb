32e520461c5032ce3c88644f40aedd0c6762ffe7
nitin gupta <nitinmangala@in.com>
2012-04-30 15:11:34 +0530

    pascal traingle

--- containers_blocks_iterators/pascal_triangle.rb
+++ containers_blocks_iterators/pascal_triangle.rb
@@ -6,23 +6,30 @@ def pascal(n)
 		
 	for i in 0..n
 		if i<=1;
-			if i==0
-				puts a.first;
-			else		
-				print a.first," ",a.last,"\n";
-			end
-
+			yield i,a;
 		else
-			print a.first," ";
+			yield i,a.first
 			for j in 0...a.length-1
-				print a[j]+a[j+1]," ";
+				yield i,a[j]+a[j+1];
 				b[j+1] = a[j]+a[j+1];
 			end		
-			puts a.last;
+			yield false,a.last;
 			b[b.length] = 1;
 			a=b.dup;
 		end
 	end
 end
 
-pascal(6);
+pascal(6) do |i,val|
+	if i==0
+		puts val.first;
+	elsif i==1
+		print val.first," ",val.last,"\n";
+	else
+		if i
+			print val," ";
+		else
+		 puts val;
+		end
+	end
+end
