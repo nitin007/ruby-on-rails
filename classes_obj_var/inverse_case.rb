92c213e06747060ef9bac251295ae1e32753eda2
nitin gupta <nitinmangala@in.com>
2012-05-02 13:16:30 +0530

    swapping the cases of string(changes done)

--- classes_obj_var/inverse_case.rb
+++ classes_obj_var/inverse_case.rb
@@ -1,16 +1,9 @@
 # Again you haven't read the API carefully..
 class String
 	def to_s
-		i=0;
-		self.each_char do |c|
-			if c.match(/[a-z]/)
-				yield c.upcase;
-			else
-				yield c.downcase;
-			end
-		end
+		self.swapcase
 	end
 end
 
-"hello WORLD".to_s {|c| print c;}
+p "hello WORLD".to_s
 
