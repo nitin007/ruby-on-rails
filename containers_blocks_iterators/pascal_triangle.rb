fe62383f3c8fc35582cc14b15f3621955eb02a18
nitin gupta <nitinmangala@in.com>
2012-05-02 15:57:48 +0530

    pascal triangle

--- containers_blocks_iterators/pascal_triangle.rb
+++ containers_blocks_iterators/pascal_triangle.rb
@@ -1,35 +1,25 @@
 def pascal(n)
-	a=[];
-	b=[];
-	a.push(1,1);
-	b.push(1);
+	currentarr = [1,1]
+	nextarr = [1]
 		
 	for i in 0..n
-		if i<=1;
-			yield i,a;
+		if i == 0
+			yield currentarr.first,"\n"
+		elsif i == 1
+			yield currentarr.first," ",currentarr.last,"\n"
 		else
-			yield i,a.first
-			for j in 0...a.length-1
-				yield i,a[j]+a[j+1];
-				b[j+1] = a[j]+a[j+1];
+			yield currentarr.first," "
+			for j in 0...currentarr.length-1
+				yield currentarr[j]+currentarr[j+1]," "
+				nextarr[j+1] = currentarr[j]+currentarr[j+1]
 			end		
-			yield false,a.last;
-			b[b.length] = 1;
-			a=b.dup;
+			yield currentarr.last,"\n"
+			nextarr[nextarr.length] = 1
+			currentarr = nextarr.dup
 		end
 	end
 end
 
-pascal(6) do |i,val|
-	if i==0
-		puts val.first;
-	elsif i==1
-		print val.first," ",val.last,"\n";
-	else
-		if i
-			print val," ";
-		else
-		 puts val;
-		end
-	end
+pascal(6) do |*args|
+	args.each {|arg| print arg}
 end
