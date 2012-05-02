bf6ac173eb6a771c295198d9b2f405dde5aa775f
nitin gupta <nitinmangala@in.com>
2012-05-02 15:57:06 +0530

    group the array of strings into two(even & odd)length

--- containers_blocks_iterators/hash_inject.rb
+++ containers_blocks_iterators/hash_inject.rb
@@ -1,30 +1,31 @@
 class Array
 	def hash_inject
-		ath=Hash.new;
+		ath = {}
 		
-		self.each do |	x|
-			if	!ath[x.to_s.length]
-				ath[x.to_s.length]=[];
-				ath[x.to_s.length].push(x);
+		self.each do |x|
+			len = x.to_s.length
+			if	!ath[len]
+				ath[len]=[]
+				ath[len].push(x)
 			else
-				ath[x.to_s.length].push(x);
+				ath[len].push(x)
 			end
 		end
 		
-		hsh_inj=Hash.new;
-		hsh_inj['odd']=[];
-		hsh_inj['even']=[];
+		hsh_inj = {}
+		hsh_inj['odd']=[]
+		hsh_inj['even']=[]
 		
 		ath.inject(0) do |c,(key,val)|
 			if key%2==1
-				hsh_inj['odd'].push(ath[key]);
+				hsh_inj['odd'].push(ath[key])
 			else
-				hsh_inj['even'].push(ath[key]);
+				hsh_inj['even'].push(ath[key])
 			end
 		end
-		return hsh_inj;
+		hsh_inj
 	end
 end
 
-puts ["abc","def",1234,234,"abcd","x","mnop",5,"zZzZ"].hash_inject;
+p ["abc","def",1234,234,"abcd","x","mnop",5,"zZzZ"].hash_inject
 
