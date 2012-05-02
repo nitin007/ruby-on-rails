6b89336f678e6533007fb648337fb3c5b2ac773c
nitin gupta <nitinmangala@in.com>
2012-05-02 16:08:26 +0530

    fibonacci by recursion(changes done)

--- RUBY-New/fib_rec.rb
+++ RUBY-New/fib_rec.rb
@@ -1,12 +1,14 @@
 ## Dont need to write return
+
+#changes done
 def fib_rec(n)
 	if(n==1||n==2)
-		return 1;
+		1
 	else
-		return fib_rec(n-2)+fib_rec(n-1);
+		fib_rec(n-2)+fib_rec(n-1)
 	end
 end
 
 for i in 1..10
-	print fib_rec(i)," ";
+	p fib_rec(i)
 end
