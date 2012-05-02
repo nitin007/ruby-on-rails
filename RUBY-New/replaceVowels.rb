50b2ea4f1f795123052e8766d90d23fcddaad244
nitin gupta <nitinmangala@in.com>
2012-05-02 16:11:43 +0530

    replace vowels with *(changes done)

--- RUBY-New/replaceVowels.rb
+++ RUBY-New/replaceVowels.rb
@@ -1,7 +1,7 @@
 def replace_vowels(text)
-	text.gsub(/[aeiou]/,"*");
+	text.gsub(/[aeiou]/,"*")
 end
 
-print "Enter some text:";
-text = gets.chomp;
-p replace_vowels(text);
+print "Enter some text:"
+text = gets.chomp
+p replace_vowels(text)
