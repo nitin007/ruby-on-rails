0c01e74ab719677a06b9556792a566708738a4b5
nitin gupta <nitinmangala@in.com>
2012-05-02 16:09:33 +0530

    palindrome

--- RUBY-New/palindrome.rb
+++ RUBY-New/palindrome.rb
@@ -1,35 +1,30 @@
 def palindrome
-	print "Enter any word:";
-	word = gets.chomp;
+	print "Enter any word:"
+	word = gets.chomp
 	if word=='q'||word=='Q'
     # use exit. 'return' will return back to where the function was called from. Doesn't exit the program. Please also read up the difference between
     # exit and abort
-		return;
+		exit
 	end
 	while word.length>0
 		if word[0]==word[-1]
 			if word.length==1 || word.length==2
 				word="";
- 				yield 1;
+ 				yield "yes, It's a palindrome!"
 			else
 				word = word[1...-1]		
 			end				
 		else
-			word="";			
+			word=""
+			#since we want to execute untill user enters q/Q return will abort the program
+			yield "Not a palindrome!"
 			# Why are we using yield ??????????? I think you just want to return
-			yield false;
 		end
 	end	
-	wrap;
+	wrap
 end
 
 def wrap
-	palindrome do |f|
-		if f
-			p "yes, It's a palindrome!";
-		else
-			p "Not a palindrome!";
-		end
-	end
+	palindrome{|msg| p msg}
 end
-wrap;
+wrap
