3a5df00a10cb03f0928c30c0c40b792270cf8749
nitin gupta <nitinmangala@in.com>
2012-05-02 16:15:13 +0530

    prime nos using step function

--- standard types/prime_step.rb
+++ standard types/prime_step.rb
@@ -1,18 +1,18 @@
 def prime_nums(n)
-	print "2 ";
-	i = 2;
-	while i<=n
-		(2...i).step() do |x|
-			if i%x!=0
-				if x==i-1
-					yield i;
+	print "2 "
+	i = 2
+	while i <= n
+		(2...i).step do |x|
+			if i%x != 0
+				if x == i-1
+					yield i
 				end
 			else
-				break;
+				break
 			end
 		end
-		i+=1;
+		i += 1
 	end
 end
 
-puts prime_nums(100){|i| print i," ";}
+p prime_nums(100){|i| print i," "}
