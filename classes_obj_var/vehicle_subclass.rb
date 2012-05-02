d586144c428f6250398cb64240e478d2b58b4981
nitin gupta <nitinmangala@in.com>
2012-05-02 13:24:58 +0530

    vehicle-bike subclass

--- classes_obj_var/vehicle_subclass.rb
+++ classes_obj_var/vehicle_subclass.rb
@@ -1,29 +1,27 @@
 # By convention attr_reader/writer/accessor statements are first statements after opening a class
 # Please stop using ';' at the end of a statement, unless you want to write multiple statements in a single line.
 # Why have you still used 'print "/n".. we talked about using 'puts'
+
+#changes done
 class Vehicle
-  attr_reader :name, :price, :dealer;
+  attr_accessor :name, :price, :dealer
 	
 	def initialize(name,price)
-		@name = name;
-		@price = price;
-	end
-	
-
-	def change_price=(price)
+		@name = name
 		@price = price
 	end
+
 end
 
 class Bike < Vehicle
 	def initialize(name,price,dealer)
-		super(name,price);
+		super(name,price)
 		@dealer = dealer
 	end
 end
 
-hero = Bike.new("suplendor",25000,"Alok Motors");
-print hero.name,"\t",hero.price,"\t",hero.dealer,"\n";
-hero.change_price = 30000;
-print hero.name,"\t",hero.price,"\t",hero.dealer,"\n";
+hero = Bike.new("suplendor",25000,"Alok Motors")
+p "#{hero.name} #{hero.price} #{hero.dealer}"
+hero.price = 30000
+p "#{hero.name} #{hero.price} #{hero.dealer}"
 
