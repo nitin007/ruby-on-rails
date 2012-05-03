638d5f38b88e393b8183aa47471ac847fc661de5
nitin gupta <nitinmangala@in.com>
2012-05-03 11:59:10 +0530

    add multiple time values

---
+++ More_abt_methods/sum_mulitiple_times.rb
@@ -0,0 +1,51 @@
+def view(str)
+	p str
+end
+
+
+def resulting_time(hrs,mins,secs)
+	r_d = 0	
+	
+	total_hrs = hrs.inject(0) {|sum,ele| sum+ele}
+	total_mins = mins.inject(0) {|sum,ele| sum+ele}
+	total_secs = secs.inject(0) {|sum,ele| sum+ele}
+	
+	r_s = total_secs%60
+	total_mins += total_secs/60.abs
+	
+	r_m = total_mins%60
+	total_hrs += total_mins/60.abs
+	
+	r_h = total_hrs%24
+	r_d +=  total_hrs/24.abs
+	
+	view "#{r_d} day & #{r_h}:#{r_m}:#{r_s}"
+end
+
+
+def add_times(*args)
+	hrs = []
+	mins = []
+	secs = []
+	
+	args.each do |time|
+		if time.match(/^(\d{1,2})?:(\d{1,2})?:(\d{1,2})$/)
+			if $1.to_i >= 24 || $2.to_i >= 60 || $3.to_i >= 60
+				view "values out of range!"
+				exit
+			else
+				hrs.push($1.to_i)
+				mins.push($2.to_i)
+				secs.push($3.to_i)
+			end
+		else
+			view "Values not valid!"
+			exit
+		end
+	end
+	resulting_time(hrs,mins,secs)
+end
+
+add_times("11:23:07","22:53:45","0:23:23","23:45:56")
+
+
